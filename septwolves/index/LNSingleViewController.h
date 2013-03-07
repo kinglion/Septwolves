138102a996a05d9706a9793cbce97745a8ca8b69
kinglion <sangerenba@gmail.com>
2013-03-08 01:21:07 +0800

    2013-03-08
    UIPageControl

--- septwolves/index/LNSingleViewController.h
+++ septwolves/index/LNSingleViewController.h
@@ -10,11 +10,16 @@
 #import "SBTableAlert.h"
 #import "FPPopoverController.h"
 
-@interface LNSingleViewController : UIViewController<FPPopoverControllerDelegate,SBTableAlertDataSource,SBTableAlertDelegate>
+@interface LNSingleViewController : UIViewController<FPPopoverControllerDelegate,SBTableAlertDataSource,SBTableAlertDelegate,UIScrollViewDelegate>
 {
     UIPageControl *pageControl;
     UIScrollView *scrollView;
+    NSString *str;
+    NSArray *array;
+    NSInteger currentNum;
 }
 @property (nonatomic,retain) UIPageControl *pageControl;
 @property (nonatomic,retain) UIScrollView *scrollView;
+@property (nonatomic,copy) NSArray *array;
+@property (nonatomic) NSInteger currentNum;
 @end
--- septwolves/index/LNSingleViewController.m
+++ septwolves/index/LNSingleViewController.m
@@ -8,7 +8,7 @@
 
 #import "LNSingleViewController.h"
 #import "FPPopoverController.h"
-
+#import "JSONKit.h"
 
 @interface LNSingleViewController ()
 
@@ -20,12 +20,14 @@
 @synthesize tarBtn;
 @synthesize pageControl;
 @synthesize scrollView = _scrollView;
+@synthesize array = _array;
+@synthesize currentNum;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         self.title = @"西装服饰";
-        
+        [self.view setBackgroundColor:[UIColor blackColor]];
         //navigationBarItem
         UIImage* backImage = [UIImage imageNamed:@"backButton.png"];
         CGRect backframe = CGRectMake(0,0,30,19);
@@ -59,32 +61,70 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    pageControl = [[UIPageControl alloc]init];
-    [pageControl setCenter:CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height - 20)];
-    [pageControl setNumberOfPages:1];
-    // Custom initialization
+    currentNum = 1;
+    str = @"{\"result\":\"000\", \"imgArr\":[\"http://img.itc.cn/photo/jejJ6dJCVWW\",\"http://www.fzlol.com/upimg/allimg/130226/2132T91491.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T95612.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T96443.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T96443.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T96443.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T96443.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T96443.jpg\",\"http://www.fzlol.com/upimg/allimg/130226/2132T96443.jpg\"]}";
+    NSDictionary *data = [str objectFromJSONString];
+    _array = [data objectForKey:@"imgArr"];
+    int arrCount = [_array count];
     _scrollView = [[UIScrollView alloc]initWithFrame:self.view.frame];
     [_scrollView setUserInteractionEnabled:YES];
+    [_scrollView setPagingEnabled:YES];
+    [_scrollView setDelegate:self];
+    [_scrollView setContentSize:CGSizeMake(self.view.frame.size.width * arrCount, self.view.frame.size.height)];
     [self.view addSubview:_scrollView];
-    UIImage *image = [UIImage imageNamed:@"man1.png"];
-    UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
-    [imageView setFrame:self.view.frame];
-    //[_scrollView addSubview:imageView];
-    //生成触摸点
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setImage:[UIImage imageNamed:@"03.png"] forState:UIControlStateNormal];
-    [button setFrame:CGRectMake(self.view.frame.size.width - 74,10, 64, 64)];
-    [button addTarget:self action:@selector(onClickPop:) forControlEvents:UIControlEventTouchUpInside];
-    [button setBackgroundColor:[UIColor blackColor]];
-    [_scrollView addSubview:button];
+    for (int i = 0; i < arrCount; i++) {
+        UIImage *image = [UIImage imageNamed:@"man1.png"];
+        UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+        [imageView setFrame:CGRectMake(self.view.frame.size.width * i, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        [_scrollView addSubview:imageView];
+        [imageView setUserInteractionEnabled:YES];
+        //生成触摸点
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [button setImage:[UIImage imageNamed:@"03.png"] forState:UIControlStateNormal];
+        [button setFrame:CGRectMake(self.view.frame.size.width - 74,10, 64, 64)];
+        [button addTarget:self action:@selector(onClickPop:) forControlEvents:UIControlEventTouchUpInside];
+        [imageView addSubview:button];
+        [imageView release];
+        //[button release];
+        //
+        //[image release];
+    }
     
+    // Custom initialization
     //addSubView UIpageControl
-    //[self.view addSubview:pageControl];
-    [button release];
-    //[imageView release];
-    //[image release];
+    pageControl = [[UIPageControl alloc]init];
+    [pageControl setCurrentPage:0];
+    [pageControl setNumberOfPages:arrCount];
+    [pageControl setFrame:CGRectMake((self.view.frame.size.width - pageControl.frame.size.width)*0.5,self.view.frame.size.height - 30.0f,16*(arrCount-1)+16,16)];
+    [pageControl setCenter:CGPointMake(self.view.center.x, self.view.frame.size.height - 40.0f)];
+    [pageControl setBounds:CGRectMake(0,0,16*(arrCount-1)+16,16)]; //页面控件上的圆点间距基本在16左右。
+    [pageControl setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.2]];
+    [pageControl.layer setCornerRadius:8]; // 圆角层
+    [self.view addSubview:pageControl];
     [_scrollView release];
     // Do any additional setup after loading the view from its nib.
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)ascrollView
+{
+	CGFloat pageWidth = ascrollView.bounds.size.width ;
+    float fractionalPage = ascrollView.contentOffset.x / pageWidth ;
+	NSInteger nearestNumber = lround(fractionalPage) ;
+	
+	if (pageControl.currentPage != nearestNumber)
+	{
+		pageControl.currentPage = nearestNumber ;
+		
+		// if we are dragging, we want to update the page control directly during the drag
+		if (ascrollView.dragging)
+			[pageControl updateCurrentPageDisplay] ;
+	}
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)aScrollView
+{
+	// if we are animating (triggered by clicking on the page control), we update the page control
+	[pageControl updateCurrentPageDisplay] ;
 }
 
 - (void)doClickBackAction:(id)sender
